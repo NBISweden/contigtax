2ab7ac9dcbdde9e7bcf9b53a2515989d08228079
johnne <john.sundh@scilifelab.se>
2019-02-06 14:55:39 +0100

    Add support for multiprocessing. Use tqdm for progress bar.

--- tango/assign.py
+++ tango/assign.py
@@ -11,11 +11,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from ete3 import NCBITaxa
 import pandas as pd
-import subprocess
-import os
+from multiprocessing import Pool
+import tqdm
 import sys
+import gzip as gz
+from tango.prepare import init_sqlite_taxdb
 
 
 def calculate_norm_id(df):
@@ -31,17 +32,28 @@ def calculate_norm_id(df):
 
 def get_thresholds(df, top=10):
     """Get score threshold for each query"""
-    thresholds = (df.sort_values("bitscore", ascending=False).groupby(level=0).first().bitscore*((100-top))/100).to_dict()
+    thresholds = (df.sort_values("bitscore", ascending=False).groupby(level=0).first().bitscore * (
+        (100 - top)) / 100).to_dict()
     return thresholds
 
 
+def get_rank_thresholds(args):
+    """Checks rank thresholds and creates dictionary to hold them"""
+    rank_thresholds = {}
+    assert (len(args.rank_thresholds) == len(args.ranks))
+    for i, rank in enumerate(args.ranks):
+        rank_thresholds[rank] = args.rank_thresholds[i]
+    return rank_thresholds
+
+
 def add_names(x, taxid, ncbi_taxa):
-    names = ncbi_taxa.get_taxid_translator(list(x.loc[taxid].values)+[taxid])
+    """Adds taxa names for taxonomy ids"""
+    names = ncbi_taxa.get_taxid_translator(list(x.loc[taxid].values) + [taxid])
     n = {}
     for rank in list(x.columns):
-        t = x.loc[taxid,rank]
-        if t<0:
-            name = "{}.{}".format(names[-t],rank)
+        t = x.loc[taxid, rank]
+        if t < 0:
+            name = "{}.{}".format(names[-t], rank)
         else:
             name = names[t]
         n["{}.name".format(rank)] = name
@@ -50,46 +62,17 @@ def add_names(x, taxid, ncbi_taxa):
     return pd.merge(x, name_df, left_index=True, right_index=True)
 
 
-def propagate_lower(x,taxid,ranks):
+def propagate_lower(x, taxid, ranks):
+    """Propagates taxonomy to lower ranks"""
     rev_ranks = [ranks[x] for x in list(range(len(ranks) - 1, -1, -1))]
     missing = {}
     known = taxid
     for rank in rev_ranks[1:]:
-        if not rank in x.columns:
+        if rank not in x.columns:
             missing[rank] = -known
         else:
-            known = x.loc[taxid,rank]
-    return pd.merge(x, pd.DataFrame(missing,index=[taxid]), left_index=True, right_index=True)
-
-
-def get_lineage_df(res, ncbi_taxa, ranks):
-    lineages = ncbi_taxa.get_lineage_translator(res.staxids.unique())
-    sys.stderr.write("#Extracting lineages for {} taxids\n".format(len(lineages)))
-    lineage_df = pd.DataFrame(columns=set(ranks).difference(["species"]))
-    i = 0
-    for taxid, lineage in lineages.items():
-        i+=1
-        if i%100 == 0:
-            p = float(i) / len(lineages)*100
-            sys.stderr.write("#PROGRESS:   {}% ({}/{})\r".format(round(p,2), i, len(lineages)))
-            sys.stderr.flush()
-        lineage_ranks = ncbi_taxa.get_rank(lineage)
-        x = pd.DataFrame(lineage_ranks,index=["rank"]).T
-        x = x.loc[x["rank"].isin(ranks)].reset_index().T
-        x.columns = x.loc["rank"]
-        x.drop("rank", inplace=True)
-        x.set_index("species", inplace=True)
-        if len(x.columns)<len(ranks)-1:
-            x = propagate_lower(x, taxid, ranks)
-        x = add_names(x, taxid, ncbi_taxa)
-        lineage_df = pd.concat([lineage_df, x], sort=True)
-    lineage_df.index.name = "species"
-    lineage_df = lineage_df.assign(species=pd.Series(lineage_df.index, index=lineage_df.index))
-    sys.stderr.write("#PROGRESS:   100% ({}/{})\r".format(i,i))
-    sys.stderr.flush()
-    sys.stderr.write("\n")
-    lineage_df[ranks].to_csv("lineage_df.tsv", sep="\t")
-    return lineage_df
+            known = x.loc[taxid, rank]
+    return pd.merge(x, pd.DataFrame(missing, index=[taxid]), left_index=True, right_index=True)
 
 
 def get_lca(r, ranks):
@@ -101,7 +84,7 @@ def get_lca(r, ranks):
         higher_rank_names = ["{}.name".format(x) for x in higher_ranks]
         c = r.groupby(rank).count()
         if len(c) == 1:
-            if len(r)==1:
+            if len(r) == 1:
                 lca = r.loc[query, higher_rank_names].values
                 lca_taxids = r.loc[query, higher_ranks].values
             else:
@@ -128,6 +111,7 @@ def parse_with_rank_thresholds(r, ranks, rank_thresholds, mode, vote_threshold):
         if len(_r) == 0:
             continue
         lca = {}
+        lca_taxids = {}
         # After filtering, either calculate lca from all filtered taxids
         if mode == "rank_lca":
             lca, lca_taxids = get_lca(_r, allowed_ranks)
@@ -146,7 +130,7 @@ def get_rank_vote(r, rank, vote_threshold=0.5):
     # Create dataframe for unique taxids filtered at this rank threshold
     taxid_counts = pd.DataFrame(dict.fromkeys(r.staxids.unique(), 1), index=["count"]).T
     # Add taxid for rank being investigated
-    rank_df = r.groupby("staxids").first().reset_index()[[rank,"staxids"]].set_index("staxids")
+    rank_df = r.groupby("staxids").first().reset_index()[[rank, "staxids"]].set_index("staxids")
     rank_df = pd.merge(taxid_counts, rank_df, left_index=True, right_index=True)
     # Sum counts for current rank
     rank_sum = rank_df.groupby(rank).sum()
@@ -164,98 +148,143 @@ def propagate_taxa(res, ranks):
     for rank in ranks:
         if res[rank] == "Unclassified":
             if known != "":
-                res[rank] = "{}.{}".format("Unclassified",known)
+                res[rank] = "{}.{}".format("Unclassified", known)
         else:
             known = res[rank]
     return res
 
 
 def series2df(df):
+    """Converts pandas series to pandas dataframe"""
     if str(type(df)) == "<class 'pandas.core.series.Series'>":
         df = pd.DataFrame(df).T
     return df
 
 
-def parse_hits6(res, top, ranks, mode, rank_thresholds, vote_threshold):
-    """Parse the typical outfmt 6 blast file"""
-    thresholds = get_thresholds(res, top=top)
+def read_df(f, open_function):
+    """Reads the blast output into a format that can be processed with the multiprocessing module"""
+    r = {}
+    taxids = []
+    with open_function(f, 'rt') as fhin:
+        for i, line in enumerate(tqdm.tqdm(fhin, desc="Reading {}".format(f), ncols=100, unit=" lines")):
+            line = line.rstrip()
+            items = line.rsplit()
+            taxids.append(items[-2])
+            try:
+                r[items[0]] += [[items[1]] + [float(x) for x in items[2:]]]
+            except KeyError:
+                r[items[0]] = [[items[1]] + [float(x) for x in items[2:]]]
+    return r, list(set(taxids))
+
+
+def process_lineages(items):
+    """Looks up lineage information from taxids"""
+    taxid, ranks, taxdir, lineage = items
+    # Read the taxonomy db
+    ncbi_taxa = init_sqlite_taxdb(taxdir)
+    lineage_ranks = ncbi_taxa.get_rank(lineage)
+    x = pd.DataFrame(lineage_ranks, index=["rank"]).T
+    x = x.loc[x["rank"].isin(ranks)].reset_index().T
+    x.columns = x.loc["rank"]
+    x.drop("rank", inplace=True)
+    x.set_index("species", inplace=True)
+    if len(x.columns) < len(ranks) - 1:
+        x = propagate_lower(x, taxid, ranks)
+    x = add_names(x, taxid, ncbi_taxa)
+    return x
+
+
+def make_lineage_df(taxids, taxdir, ranks, threads=1):
+    """Adds lineage information to diamond results"""
+    # Read the taxonomy db
+    ncbi_taxa = init_sqlite_taxdb(taxdir)
+    lineages = ncbi_taxa.get_lineage_translator(taxids)
+    items = [[taxid, ranks, taxdir, lineages[taxid]] for taxid in list(lineages.keys())]
+    with Pool(processes=threads) as pool:
+        res = list(tqdm.tqdm(pool.imap(process_lineages, items), desc="Creating lineages", total=len(items), unit=" taxids", ncols=100))
+    return pd.concat(res, sort=False)
+
+
+def process_queries(items):
+    """Receives a query and its results and assigns taxonomy"""
     res_tax = {}
     res_taxids = {}
-    queries = res.index.unique()
-    if "rank" in mode:
-        # Calculate normalized id values for each hit if rank thresholds are given
-        sys.stderr.write("#Calculating normalized %id for hits\n")
-        res = calculate_norm_id(res)
-        min_rank_threshold = min([x for x in rank_thresholds.values()])
+    query, lineage_df, res, rank_thresholds, args = items
+    min_rank_threshold = min([x for x in rank_thresholds.values()])
+    # Create pandas dataframe for slice
+    res_df = pd.DataFrame(res, columns=['sseqid', 'pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend',
+                                        'sstart', 'send', 'evalue', 'bitscore', 'staxids', 'slen'],
+                          index=[query] * len(res))
+    # Calculate normalized %id for slice
+    res_df = calculate_norm_id(res_df)
+    # Calculate bit score threshold for slice
+    thresholds = get_thresholds(res_df, top=args.top)
+    # Set index
+    res_df.index.name = "qseqid"
+    # Merge with lineage df
+    res_df = pd.merge(res_df.reset_index(), lineage_df.reset_index(), left_on="staxids", right_on="species", how="left")
+    res_df.set_index("qseqid", inplace=True)
+    # Initialize dictionaries
+    res_tax[query] = dict.fromkeys(args.ranks, "Unclassified")
+    res_taxids[query] = dict.fromkeys(args.ranks, -1)
+    # Handle queries that return pandas Series
+    res_df = res_df.loc[res_df.bitscore >= thresholds[query]]
+    res_df = series2df(res_df)
+    lca = {}
+    lca_taxids = {}
+    # Parse with rank thresholds or by just filtering by bitscore
+    if "rank" in args.mode:
+        if len(res_df.loc[res_df.normid >= min_rank_threshold]) > 0:
+            lca, lca_taxids = parse_with_rank_thresholds(res_df, args.ranks, rank_thresholds, args.mode, args.vote_threshold)
     else:
-        queries = res.index.unique()
-    sys.stderr.write("#Parsing blast file\n")
-    for i, query in enumerate(queries, start=1):  # type: (int, object)
-        if i % 10 == 0:
-            p = (float(i)/len(queries))*100
-            sys.stderr.write("#PROGRESS:   {}% ({}/{})\r".format(round(p, 2), i, len(queries)))
-            sys.stderr.flush()
-        # Initialize results for query
-        res_tax[query] = dict.fromkeys(ranks, "Unclassified")
-        res_taxids[query] = dict.fromkeys(ranks, -1)
-        # Filter results for query
-        _r = res.loc[query]
-        _r = series2df(_r)
-        _r = _r.loc[_r.bitscore >= thresholds[query]]
-        _r = series2df(_r)
-        if "rank" in mode:
-            if len(_r.loc[_r.normid >= min_rank_threshold]) == 0:
-                continue
-            lca, lca_taxids = parse_with_rank_thresholds(_r, ranks, rank_thresholds, mode, vote_threshold)
-        else:
-            lca, lca_taxids = get_lca(_r, ranks)
-        res_tax[query].update(lca)
-        res_taxids[query].update(lca_taxids)
-        res_tax[query] = propagate_taxa(res_tax[query], ranks)
-    sys.stderr.write("#PROGRESS:   100% ({}/{})\n".format(i,len(queries)))
-    return res_tax, res_taxids
+        lca, lca_taxids = get_lca(res_df, args.ranks)
+    # Update results with lca and lca_taxids
+    res_tax[query].update(lca)
+    res_taxids[query].update(lca_taxids)
+    res_tax[query] = propagate_taxa(res_tax[query], args.ranks)
+    return res_tax[query], res_taxids[query], query
 
 
-def parse_hits102(taxids, ncbi_taxa,
-                  ranks=["superkingdom","kingdom","phylum","class","order","family","genus","species"]):
-    '''Parses the -102 format output from diamond'''
-    lca_dict = {}
-    for t in taxids:
-        lineage = ncbi_taxa.get_lineage(t)
-        lca_dict[t] = {}
-        for rank in ranks:
-            lca_dict[t][rank] = "Unclassified"
-        for taxid in lineage:
-            rank = ncbi_taxa.get_rank([taxid])[taxid]
-            name = ncbi_taxa.get_taxid_translator([taxid])[taxid]
-            if rank in ranks:
-                lca_dict[t][rank] = name
-        # Propagate lowest known taxonomy
-        lca_dict[t] = propagate_taxa(lca_dict[t], ranks)
-    return lca_dict
-
-
-def init_taxdb(dbfile):
-    if not dbfile:
-        ncbi_taxa = NCBITaxa()
-    else:
-        dirname = os.path.dirname(dbfile)
-        cmd = "mkdir -p {}".format(dirname)
-        p1 = subprocess.Popen(cmd, shell=True, stdin=None)
-        p1.wait()
-        cmd = "touch {}".format(dbfile)
-        p2 = subprocess.Popen(cmd, shell=True, stdin=None)
-        p2.wait()
-        ncbi_taxa = NCBITaxa(dbfile)
-    return ncbi_taxa
-
-
-def write_blobout(f, res_taxids, ranks):
+def write_blobout(f, res_taxids, queries, ranks):
+    """Writes blob-format output for use with blobtools"""
     rev_ranks = [ranks[x] for x in list(range(len(ranks) - 1, -1, -1))]
     with open(f, 'w') as fhout:
-        for query, d in res_taxids.items():
+        for i, query in enumerate(queries):
+            d = res_taxids[i]
             for rank in rev_ranks:
                 if rank in d.keys():
                     taxid = d[rank]
-                    fhout.write("{query}\t{taxid}\t1\tref\n".format(query=query, taxid=taxid))
-                    break
+                    if taxid != -1:
+                        fhout.write("{query}\t{taxid}\t1\tref\n".format(query=query, taxid=taxid))
+                        break
+
+
+def parse_hits(args):
+    """Parse the diamond blastx output"""
+    # Set up rank thresholds
+    rank_thresholds = get_rank_thresholds(args)
+    # Read diamond results
+    if (args.diamond_results).split(".")[-1] == "gz":
+        open_function = gz.open
+    else:
+        open_function = open
+    res, taxids = read_df(args.diamond_results, open_function)
+    # Create lineage dataframe
+    lineage_df = make_lineage_df(taxids, args.taxdir, args.ranks, args.threads)
+    # Set up multiprocessing pool
+    total_queries = len(res)
+    items = [[q, lineage_df, res[q], rank_thresholds, args] for q in res.keys()]
+    with Pool(processes=args.threads) as pool:
+        res = list(tqdm.tqdm(pool.imap(process_queries, items, chunksize=args.chunksize), desc="Parsing queries", total=total_queries, unit=" queries", ncols=100))
+    res_tax = [item[0] for item in res]
+    res_taxids = [item[1] for item in res]
+    queries = [item[2] for item in res]
+    # Writes blobtools-compatible output
+    if args.blobout:
+        sys.stderr.write("Writing blobtools file to {}\n".format(args.blobout))
+        write_blobout(args.blobout, res_taxids, queries, args.ranks)
+    res_df = pd.DataFrame(res_tax, index=queries)[args.ranks]
+    res_df.index.name = "query"
+    sys.stderr.write("Writing main output to {}\n".format(args.outfile))
+    res_df.to_csv(args.outfile, sep="\t")
+    return 0
